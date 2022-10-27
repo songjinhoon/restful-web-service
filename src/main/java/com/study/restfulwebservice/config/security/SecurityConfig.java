3e5c13d864d7cfa2153627fa2cd21478ab692711
hijinhoon <hijinhoon@muhanit.kr>
2022-10-27 17:53:29 +0900

    feat: h2

---
+++ src/main/java/com/study/restfulwebservice/config/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.study.restfulwebservice.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+}
